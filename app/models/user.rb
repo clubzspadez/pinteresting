783d02b2482ce71b573f49cc1dbe68fc897b50b8
Jonathan Rodriguez <clubzspadez@gmail.com>
2016-11-01 15:50:24 -0400

    db changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-#   #validates :password, :presence => true,
-#             :on => :create,
-#             :format => {:with => /\A.*(?=.{10,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\@\#\$\%\^\&\+\=]).*\Z/ }
+# validates :password, :presence => true,
+  #           :on => :create,
+  #          :format => {:with => /\A.*(?=.{10,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\@\#\$\%\^\&\+\=]).*\Z/ }
 
   has_many :pins
 
