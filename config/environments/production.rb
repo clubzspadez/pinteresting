e9d14d24293f0c585f247b12fdae21bb38d59b30
Jonathan Rodriguez <clubzspadez@gmail.com>
2016-11-03 13:01:23 -0400

    added env keys to heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,5 +87,14 @@ Rails.application.configure do
  #note to set to actual host when in production
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
+  config.paperclip_defaults = {
+  :storage => :s3,
+  :s3_credentials => {
+    :bucket => ENV['S3_BUCKET_NAME'],
+    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+  }
+}
+
 
 end
